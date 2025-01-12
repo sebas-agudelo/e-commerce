0a81debdf3f1108db6ee1b9ae0f6ced854648a69
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-01-12 22:58:53 +0100

    Shopping cart functionality on the server side

--- Server/api/routes/products/productsRouter.js
+++ Server/api/routes/products/productsRouter.js
@@ -8,7 +8,7 @@ export const productsRouter = express.Router();
 
 /* ALL PRODUCTS ROUTES */
 productsRouter.post('/api/newproduct', upload.single('img'), authenticateUser, authenticateAdmin, newProduct);
-productsRouter.get('/api/getproducts', getProducts);
+productsRouter.get('/api/products/show', getProducts);
 productsRouter.get('/api/product/get/:id', getProductByID);
 productsRouter.delete('/api/product/delete/:id', authenticateUser, authenticateAdmin, deleteProductByID);
 
