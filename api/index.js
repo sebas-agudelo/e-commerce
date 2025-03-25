427441b4de4545827922bb9d339a8765fcb7c458
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-03-25 09:35:02 +0100

    Added to vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
