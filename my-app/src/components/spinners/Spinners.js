7eafd3d21d9ba7dd2e7298a151652b15c2379da6
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-03-25 09:30:51 +0100

    Added to vercel

--- my-app/src/components/spinners/Spinners.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ClipLoader } from 'react-spinners';
-
-export default function Spinners() {
-  return (
-    <div className="loading-spinner"><ClipLoader color="#faff00" size={90}/></div>
-  )
-}
--- my-app/src/pages/Success.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Success() {
-  return (
-    <main style={{padding: "70px 0 0"}}>
-        <h1>Tack för din beställning</h1>
-    </main>
-  )
-}
