26597f9889e6a71d6e935d3f636254a2b866d004
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-03-26 20:05:20 +0100

    added frontend to vercel

--- my-app/src/components/CheckoutForm .js
+++ my-app/src/components/CheckoutForm .js
@@ -211,7 +211,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: "https://examensarbeten.vercel.app/payment-success",
+        return_url: "https://examensarbeten-luzs.vercel.app/payment-success",
       },
     });
 
