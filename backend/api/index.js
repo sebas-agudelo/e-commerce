414a6b61f62b4b328ecf247adc66879478ba63ce
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-03-26 08:47:32 +0100

    Added to vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(express.json());
 
 
-app.get("/", (req, res) => res.send("Express och vercel är bäst"));
+app.get("/", (req, res) => res.send("Express och vercel är bästtt"));
 
 app.use(authRouter);
 app.use(productsRouter);
